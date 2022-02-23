5d95f3cc651882d2e7c6b6535d4d9488fec36778
Tamaskiralyangolkiraly <99013056+Tamaskiralyangolkiraly@users.noreply.github.com>
2022-02-23 11:06:58 +0100

    hello world kiiratása

---
+++ feladat1/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Írj programot, ami kiírja a képernyőre, hogy ”Hello world!”!
+        System.out.println("hello world");
+    }
+}
