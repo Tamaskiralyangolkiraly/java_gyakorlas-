10fab8256ca3e96d2e7243665a97144e08affccb
Tamaskiralyangolkiraly <99013056+Tamaskiralyangolkiraly@users.noreply.github.com>
2022-02-24 09:03:36 +0100

    bekért számig a számok kiíratása 0-tól, egymás alá

---
+++ Feladat24/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérek egy egész számot");
+        int SZAM1 = sc.nextInt();
+        for( int i = 0; i <= SZAM1; i++ )
+        {
+            System.out.println(i);
+        }
+    }
+}
